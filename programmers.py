42ac07e36c9216e7c61c8a596350fe78ae3de523
ReasonsForWait <leekjgo21@naver.com>
2023-05-26 15:28:06 +0900

    programmers quiz

--- programmers.py
+++ programmers.py
@@ -39,8 +39,158 @@
 # 000123과 같이 0이 선행하는 경우는 없습니다.
 # 문자열에 자연수가 없는 경우 0을 return 해주세요.
 
-import re
+# TODO
+# import re
 
-my_string = "aAb1B2cC34oOp"
-for i in my_string:
-  print(re.search('\d', i))
+# my_string = "aAb1B2cC34oOp"
+# for i in my_string:
+#   print(re.search('\d', i))
+
+#-------------------------
+
+# 최솟값 만들기
+
+# 문제 설명
+# 길이가 같은 배열 A, B 두개가 있습니다. 각 배열은 자연수로 이루어져 있습니다.
+# 배열 A, B에서 각각 한 개의 숫자를 뽑아 두 수를 곱합니다. 이러한 과정을 배열의 길이만큼 반복하며, 두 수를 곱한 값을 누적하여 더합니다. 이때 최종적으로 누적된 값이 최소가 되도록 만드는 것이 목표입니다. (단, 각 배열에서 k번째 숫자를 뽑았다면 다음에 k번째 숫자는 다시 뽑을 수 없습니다.)
+
+# 예를 들어 A = [1, 4, 2] , B = [5, 4, 4] 라면
+
+# A에서 첫번째 숫자인 1, B에서 첫번째 숫자인 5를 뽑아 곱하여 더합니다. (누적된 값 : 0 + 5(1x5) = 5)
+# A에서 두번째 숫자인 4, B에서 세번째 숫자인 4를 뽑아 곱하여 더합니다. (누적된 값 : 5 + 16(4x4) = 21)
+# A에서 세번째 숫자인 2, B에서 두번째 숫자인 4를 뽑아 곱하여 더합니다. (누적된 값 : 21 + 8(2x4) = 29)
+# 즉, 이 경우가 최소가 되므로 29를 return 합니다.
+
+# 배열 A, B가 주어질 때 최종적으로 누적된 최솟값을 return 하는 solution 함수를 완성해 주세요.
+
+def s(A, B):
+    A = sorted(A)
+    B = sorted(B)[::-1]
+    
+    min_value = 0
+    for i in range(len(A)):
+        min_value += A[i] * B[i]
+
+    return min_value
+
+# s([1,2,3,2], [1,3,6,5])
+
+def solution(operations):
+    queue = []
+    for i in operations:
+        word, num = list(i.split())
+
+        if word == "I":
+            queue.append(int(num))
+        else:
+            if len(queue) != 0:
+                if num == "1":
+                    queue.remove(max(queue))
+                else:
+                    queue.remove(min(queue))
+                
+    if len(queue) == 0:
+        return [0,0]
+    else:
+        return [max(queue), min(queue)]
+    
+# solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"])
+
+#----------------------------------------------------------------------------------------------------------------
+
+# 조건에 맞게 수열 변환하기 2
+
+# 문제 설명
+# 정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 50보다 작은 홀수라면 2를 곱하고 다시 1을 더합니다.
+
+# 이러한 작업을 x번 반복한 결과인 배열을 arr(x)라고 표현했을 때, arr(x) = arr(x + 1)인 x가 항상 존재합니다. 이러한 x 중 가장 작은 값을 return 하는 solution 함수를 완성해 주세요.
+
+# 단, 두 배열에 대한 "="는 두 배열의 크기가 서로 같으며, 같은 인덱스의 원소가 각각 서로 같음을 의미합니다.
+
+# 제한사항
+# 1 ≤ arr의 길이 ≤ 1,000,000
+# 1 ≤ arr의 원소의 값 ≤ 100
+
+def solution1(arr):
+    chk = True
+    count = 0
+    while(chk):
+        new_arr = []
+        for v in arr:
+            if v >= 50 and v % 2 == 0:
+                new_arr.append(int(v / 2))
+            elif v < 50 and v % 2 == 1:
+                new_arr.append(v * 2 + 1)
+            else:
+                new_arr.append(v)
+            
+        count += 1
+        n = len(arr)
+        for i in range(len(arr)):
+            if arr[i] == new_arr[i]:
+                n -= 1
+
+            if n == 0:
+                chk = False
+                
+        arr = new_arr
+        
+    return count - 1
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# 잘라서 배열로 저장하기
+
+# 문제 설명
+# 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# 1 ≤ my_str의 길이 ≤ 100
+# 1 ≤ n ≤ my_str의 길이
+# my_str은 알파벳 소문자, 대문자, 숫자로 이루어져 있습니다.
+
+def solution3(my_str, n):
+    result = []
+    while(len(my_str) != 0):
+        result.append(my_str[:n])
+        my_str = my_str[n:]
+        
+    return result
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# 다항식 더하기
+
+# 문제 설명
+# 한 개 이상의 항의 합으로 이루어진 식을 다항식이라고 합니다. 다항식을 계산할 때는 동류항끼리 계산해 정리합니다. 
+# 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때, 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요. 
+# 같은 식이라면 가장 짧은 수식을 return 합니다.
+
+# 실패작
+
+# def solution4(polynomial):
+#     x = 0
+#     num = 0
+#     arr = polynomial.split()[::2]
+#     for i in arr:
+#         if "x" in i:
+#             print(i)
+#             if i[0] == "x":
+#                 x += 1
+#             elif i[0] == "-":
+#                 x -= int(i[1:len(i) - 1])
+#             else:
+#                 x += int(i[:len(i) - 1])
+#         else:
+#             print(i)
+#             if i[0] == "-":
+#                 num -= int(i[1:])
+#             else:
+#                 num += int(i)
+        
+#     if num != 0:
+#         return str(x) + "x + " + str(num)
+#     else:
+#         return str(x) + "x"
+
+# print(solution4("-3x + -7 + x"))
