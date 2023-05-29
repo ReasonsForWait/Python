bbc7b3a8a1c2c648fb5ae4ac3c4480d5d4ff0d74
ReasonsForWait <leekjgo21@naver.com>
2023-05-29 21:13:28 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -194,3 +194,21 @@ def solution3(my_str, n):
 #         return str(x) + "x"
 
 # print(solution4("-3x + -7 + x"))
+
+#----------------------------------------------------------------------------------------
+
+# 뒤에서 5등까지
+# https://school.programmers.co.kr/learn/courses/30/lessons/181853
+# 문제
+
+# 문제 설명
+# 정수로 이루어진 리스트 num_list가 주어집니다. num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# 6 ≤ num_list의 길이 ≤ 30
+# 1 ≤ num_list의 원소 ≤ 100
+
+def solution(num_list):
+    return sorted(num_list)[:5]
+
+solution([1,34,5,77,9,12])
