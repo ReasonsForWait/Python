55b81eb6b775ff489aeaa012db2db9bfc2487aad
ReasonsForWait <leekjgo21@naver.com>
2023-06-01 21:45:34 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -264,3 +264,22 @@ def solution8(board):
                 count += 1
                 
     return count
+
+#---------------------------------------------------------------------------------------------------------------
+
+# 문자열이 몇번 등장하는지 세기
+
+# 문제 설명
+# 문자열 myString과 pat이 주어집니다. myString에서 pat이 등장하는 횟수를 return 하는 solution 함수를 완성해 주세요.
+
+# 제한사항
+# 1 ≤ myString ≤ 1000
+# 1 ≤ pat ≤ 10
+
+def solution9(myString, pat):
+    result = 0
+    for i in range(len(myString) - len(pat) + 1):
+        if myString[i:len(pat) + i] == pat:
+            result += 1
+            
+    return result
