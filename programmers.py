52b03cec0434b10f1ac5d5f588ed7b1be4a5d96b
ReasonsForWait <leekjgo21@naver.com>
2023-06-07 15:47:26 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -418,3 +418,49 @@ def solution12(text, anagram, sw):
             result[i] = text[v]
             
     return "".join(v for v in result)
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+# [PCCP 모의고사 #1] 외톨이 알파벳
+# 문제 설명
+# 알파벳 소문자로만 이루어진 어떤 문자열에서, 2회 이상 나타난 알파벳이 2개 이상의 부분으로 나뉘어 있으면 외톨이 알파벳이라고 정의합니다.
+
+# 문자열 "edeaaabbccd"를 예시로 들어보면,
+
+# a는 2회 이상 나타나지만, 하나의 덩어리로 뭉쳐있으므로 외톨이 알파벳이 아닙니다.
+# "ede(aaa)bbccd"
+# b, c도 a와 같은 이유로 외톨이 알파벳이 아닙니다.
+# d는 2회 나타나면서, 2개의 부분으로 나뉘어 있으므로 외톨이 알파벳입니다.
+# "e(d)eaaabbcc(d)"
+# e도 d와 같은 이유로 외톨이 알파벳입니다.
+# 문자열 "eeddee"를 예시로 들어보면,
+
+# e는 4회 나타나면서, 2개의 부분으로 나뉘어 있으므로 외톨이 알파벳입니다.
+# "(ee)dd(ee)"
+# d는 2회 나타나지만, 하나의 덩어리로 뭉쳐있으므로 외톨이 알파벳이 아닙니다.
+# "ee(dd)ee"
+# 문자열 input_string이 주어졌을 때, 외톨이 알파벳들을 알파벳순으로 이어 붙인 문자열을 return 하도록 solution 함수를 완성해주세요. 만약, 외톨이 알파벳이 없다면 문자열 "N"을 return 합니다.
+
+# 제한사항
+# 1 ≤ input_string의 길이 ≤ 2,600
+# input_string은 알파벳 소문자로만 구성되어 있습니다.
+
+def solution13(input_string):
+    result = []
+    result_dic = {}
+    for i, v in enumerate(input_string):
+        if v not in result_dic:
+            result_dic[v] = 1
+        else:
+            result_dic[v] += 1
+        
+        if i == 0:
+            continue
+            
+        if result_dic[v] > 1 and input_string[i] != input_string[i-1]:
+            result.append(v)
+            
+    if len(result) == 0:
+        return "N"
+    else:
+        return "".join(i for i in set(sorted(result)))
