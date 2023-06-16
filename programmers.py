97e80f6d890b8996b70302dd38aebe430be7d1d7
ReasonsForWait <leekjgo21@naver.com>
2023-06-16 12:32:12 +0900

    programmers 문제풀이

--- programmers.py
+++ programmers.py
@@ -497,8 +497,18 @@ def solution14(myString, pat):
 
 # 남은것이 어느 수 인지 확인하는 프로그램을 작성하라.
 
+
 def solution15():
-    return
+    item = [0] * 99
+    for i in range(99):
+        item[i] = i + 1
+
+    total = 5050
+
+    for i in item:
+        total -= item[i]
+
+    return total
 
 
 #--------------------------------------------------------------------------------------------------
@@ -526,3 +536,32 @@ print(solution16("adaa"))
 
 def solution17():
     return
+
+#------------------------------------------------------------------------------------------------------
+
+# 외계어 사전
+
+# 문제 설명
+# PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다. 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다. spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+# spell과 dic의 원소는 알파벳 소문자로만 이루어져있습니다.
+# 2 ≤ spell의 크기 ≤ 10
+# spell의 원소의 길이는 1입니다.
+# 1 ≤ dic의 크기 ≤ 10
+# 1 ≤ dic의 원소의 길이 ≤ 10
+# spell의 원소를 모두 사용해 단어를 만들어야 합니다.
+# spell의 원소를 모두 사용해 만들 수 있는 단어는 dic에 두 개 이상 존재하지 않습니다.
+# dic과 spell 모두 중복된 원소를 갖지 않습니다.
+
+from itertools import permutations
+
+def solution18(spell, dic):
+    
+    for permutation in permutations(spell):
+        str1 = ''.join(permutation)
+        for i in dic:
+            if i == str1:
+                return 1
+            
+    return 2
