c4389a71b79b1c33ba13fcd8060992e802f15ea9
ReasonsForWait <leekjgo21@naver.com>
2023-06-21 22:22:15 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -577,7 +577,7 @@ def solution18(spell, dic):
 # 0 < A의 길이 = B의 길이 < 100
 # A, B는 알파벳 소문자로 이루어져 있습니다.
 
-def solution(A, B):
+def solution19(A, B):
     count = 0
     for i in range(len(A)):
         if (A[len(A) - i:] + A[:len(A) - i]) == B:
@@ -586,3 +586,31 @@ def solution(A, B):
         count += 1
         
     return -1
+
+#------------------------------------------------------------------------------------------------------------------------------
+
+# 치킨 쿠폰
+
+# TODO 고치기
+
+# 문제 설명
+# 프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+
+# chicken은 정수입니다.
+# 0 ≤ chicken ≤ 1,000,000
+
+def solution20(chicken):
+    result = 0
+    ticket = 0
+    while(chicken // 10 != 0):
+        result += chicken // 10
+        ticket += chicken % 10
+        chicken = chicken // 10
+        
+    ticket += chicken
+    result += ticket // 10
+        
+    print(result, chicken, ticket)
+    return result
