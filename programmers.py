4e12cb14115e7d253e6c3d35c1af114ee6283909
ReasonsForWait <leekjgo21@naver.com>
2023-06-24 13:43:39 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -614,3 +614,23 @@ def solution20(chicken):
         
     print(result, chicken, ticket)
     return result
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# 특별한 이차원 배열 1
+
+# 문제 설명
+# 정수 n이 매개변수로 주어질 때, 다음과 같은 n × n 크기의 이차원 배열 arr를 return 하는 solution 함수를 작성해 주세요.
+
+# arr[i][j] (0 ≤ i, j < n)의 값은 i = j라면 1, 아니라면 0입니다.
+# 제한사항
+# 1 ≤ n ≤ 100
+
+def solution21(n):
+    result = []
+    for i in range(n):
+        arr = [0] * n
+        arr[i] = 1
+        result.append(arr)
+        
+    return result
