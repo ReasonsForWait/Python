4d70004002c9605ff7115354dd6a9ac7732a1b9e
ReasonsForWait <leekjgo21@naver.com>
2023-07-02 20:53:01 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -632,3 +632,37 @@ def solution21(n):
         result.append(arr)
         
     return result
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# 등수 매기기
+
+# 문제 설명
+# 영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때, 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+
+# 0 ≤ score[0], score[1] ≤ 100
+# 1 ≤ score의 길이 ≤ 10
+# score의 원소 길이는 2입니다.
+# score는 중복된 원소를 갖지 않습니다.
+
+def solution22(score):
+    result = []
+    obj = {}
+    rank = []
+    for i in score:
+        result.append((i[0] + i[1]) / 2)
+    
+    
+    for i, v in enumerate(sorted(result)[::-1]):
+        j = 1
+        if v not in obj:
+            obj[v] = i + j
+        else:
+            j += 1
+
+    for i in result:
+        rank.append(obj[i])
+        
+    return rank
