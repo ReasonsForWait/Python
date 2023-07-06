6fb87206d6a54d1b897b166f7010063f494dc285
ReasonsForWait <leekjgo21@naver.com>
2023-07-06 15:09:31 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -666,3 +666,35 @@ def solution22(score):
         rank.append(obj[i])
         
     return rank
+
+#------------------------------------------------------------------------------------------------------------------------------
+
+# 특이한 정렬
+
+# 문제 설명
+# 정수 n을 기준으로 n과 가까운 수부터 정렬하려고 합니다. 이때 n으로부터의 거리가 같다면 더 큰 수를 앞에 오도록 배치합니다. 정수가 담긴 배열 numlist와 정수 n이 주어질 때 numlist의 원소를 n으로부터 가까운 순서대로 정렬한 배열을 return하도록 solution 함수를 완성해주세요.
+
+# 제한사항
+
+# 1 ≤ n ≤ 10,000
+# 1 ≤ numlist의 원소 ≤ 10,000
+# 1 ≤ numlist의 길이 ≤ 100
+# numlist는 중복된 원소를 갖지 않습니다.
+
+def solution23(numlist, n):
+    arr1 = []
+    result = []
+    for i in numlist:
+        arr1.append(i - n)
+        
+    arr2 = list(map(abs, arr1))
+    
+    for i in sorted(arr2):
+        if n + i in numlist:
+            result.append(n + i)
+            numlist.remove(n + i)
+        else:
+            result.append(n - i)
+            numlist.remove(n - i)
+            
+    return result
