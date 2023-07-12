9c582031e84e3a717fad946bbcee54b6ab7244cd
ReasonsForWait <leekjgo21@naver.com>
2023-07-12 23:27:10 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -741,3 +741,35 @@ def solution25(lines):
     return count
 
 #-----------------------------------------------------------------------------
+
+# 저주의 숫자 3
+
+# 문제 설명
+# 3x 마을 사람들은 3을 저주의 숫자라고 생각하기 때문에 3의 배수와 숫자 3을 사용하지 않습니다. 3x 마을 사람들의 숫자는 다음과 같습니다.
+
+# 정수 n이 매개변수로 주어질 때, n을 3x 마을에서 사용하는 숫자로 바꿔 return하도록 solution 함수를 완성해주세요.
+
+# 고치기
+
+def solution26(i):
+    for n in range(1, 20):
+        print(n)
+        answer = n + (n // 3)
+        if answer % 3 == 0:
+            answer += 1
+
+        def isInThree(i):
+            for n in str(i):
+                if n == "3":
+                    return True
+
+            return False
+        print(f"전 answer = {answer}")
+
+        while(isInThree(answer)):
+            answer += 1
+        
+        print(f"후 answer = {answer}")
+        
+    return answer
+
