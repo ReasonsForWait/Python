746f53486ae00eebf50f5be1589bc66dc680c137
ReasonsForWait <leekjgo21@naver.com>
2023-07-14 22:29:24 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -741,3 +741,22 @@ def solution25(lines):
     return count
 
 #-----------------------------------------------------------------------------
+
+# 간단한 식 계산하기
+
+# 문제 설명
+# 문자열 binomial이 매개변수로 주어집니다. binomial은 "a op b" 형태의 이항식이고 a와 b는 음이 아닌 정수, op는 '+', '-', '*' 중 하나입니다. 주어진 식을 계산한 정수를 return 하는 solution 함수를 작성해 주세요.
+
+# 제한사항
+# 0 ≤ a, b ≤ 40,000
+# 0을 제외하고 a, b는 0으로 시작하지 않습니다.
+
+def solution26(binomial):
+    arr = binomial.split()
+    print(arr)
+    if arr[1] == '+':
+        return int(arr[0]) + int(arr[2])
+    elif arr[1] == '-':
+        return int(arr[0]) - int(arr[2])
+    else:
+        return int(arr[0]) * int(arr[2])
