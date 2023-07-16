00309be9e0ae024594cfc18a689dd2c9d1423231
ReasonsForWait <leekjgo21@naver.com>
2023-07-16 23:40:07 +0900

    programmers

--- programmers.py
+++ programmers.py
@@ -773,3 +773,41 @@ def solution26(i):
         
     return answer
 
+#---------------------------------------------------------------------------------------------------------------------------
+
+# 유한소수 판별하기
+
+# 문제 설명
+# 소수점 아래 숫자가 계속되지 않고 유한개인 소수를 유한소수라고 합니다. 분수를 소수로 고칠 때 유한소수로 나타낼 수 있는 분수인지 판별하려고 합니다. 유한소수가 되기 위한 분수의 조건은 다음과 같습니다.
+
+# 기약분수로 나타내었을 때, 분모의 소인수가 2와 5만 존재해야 합니다.
+# 두 정수 a와 b가 매개변수로 주어질 때, a/b가 유한소수이면 1을, 무한소수라면 2를 return하도록 solution 함수를 완성해주세요.
+
+# 고치기
+
+def solution27(a, b):
+    a_arr = []
+    b_arr = []
+    for i in range(a, 1, -1):
+        if a % i == 0:
+            a_arr.append(i)
+    
+    for i in range(b, 1, -1):
+        if b % i == 0:
+            b_arr.append(i)
+    
+    common = set(a_arr) & set(b_arr)
+    if len(common) == 0:
+        for i in range(b, 1, -1):
+            if b % i == 0:
+                if i != 2 or i != 5:
+                    return 2
+                else:
+                    return 1
+    else:
+        for i in range(b // max(common), 1, -1):
+            if (b // max(common)) % i == 0:
+                if i != 2 or i != 5:
+                    return 2
+                else:
+                    return 1
